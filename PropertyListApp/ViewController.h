a0eb4e4e9d05fad4f6585faec4d3d44d904a60b6
Saleh Albatati <0xack13@gmail.com>
2014-11-07 13:47:45 +0300

    Initial Commit

---
+++ PropertyListApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PropertyListApp
+//
+//  Created by 0xack13 on 11/7/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PropertyListApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PropertyListApp
+//
+//  Created by 0xack13 on 11/7/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
