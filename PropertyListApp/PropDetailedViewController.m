4c2454033e9ad6391b49e43ab498a37079201317
Saleh Albatati <0xack13@gmail.com>
2014-11-14 17:50:45 +0300

    Add new detailed view

---
+++ PropertyListApp/PropDetailedViewController.m
@@ -0,0 +1,38 @@
+//
+//  PropDetailedViewController.m
+//  PropertyListApp
+//
+//  Created by 0xack13 on 11/14/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import "PropDetailedViewController.h"
+
+@interface PropDetailedViewController ()
+
+@end
+
+@implementation PropDetailedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
